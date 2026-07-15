ae3e1e3befac6805ee4fd3504ee4f8e7d476f8ba
PriyanshT <priyanshthakar@gmail.com>
2026-07-15 19:39:14 -0400

    coffee class added

--- js/coffee.js
+++ js/coffee.js
@@ -1,28 +1,72 @@
 const output = document.querySelector("#output");
 
 /* STEP 1: Instead of a constructor function, let's try a JavaScript class called 'Coffee' */
+class Coffee {
+    // variables/fields
+    size;
+    isDecaf;
 
-// add a serveIt method
+    // constructor
+    constructor(size, isDecaf) {
+        this.size = size;
+        this.isDecaf = isDecaf;
+    }
 
-// Generate an IMG of the coffee ordered
+    // functions/methods
+    // add a serveIt method
+    serveIt() {
+        // Generate an IMG of the coffee ordered
+        let cup = document.createElement("img"); // <img>
 
-// Set the src path for the IMG element
+        // Set the src path for the IMG element
+        cup.setAttribute("src", "images/coffee-cup.svg"); // <img src="images/coffee-cup.svg">
 
-// Determine caffeine status of the coffee
+        // Determine caffeine status of the coffee
+        if (this.isDecaf) {
+            cup.setAttribute("src", "images/coffee-cup-green.svg"); // <img src="images/coffee-cup-green.svg">
+        } else {
+            cup.setAttribute("src", "images/coffee-cup-purple.svg"); // <img src="images/coffe-cup-purple.svg">
+        }
 
-// Set the size of the cup SVG image based on this.size
+        // Set the size of the cup SVG image based on this.size
+        switch (this.size) {
+            // Size the IMG in terms of its height based on above number from the switch
+            case "small":
+                cup.setAttribute("height", 100); // <img src="images/coffee-cup.svg" height=100>
+                break;
+            case "medium":
+                cup.setAttribute("height", 150);
+                break;
+            case "large":
+                cup.setAttribute("height", 200);
+                break;
+            default:
+                cup.setAttribute("height", 150);
+        }
 
-// Size the IMG in terms of its height based on above number from the switch
+        // Generate a description of the coffee and put it into the IMG title attribute
+        // A small caffinated coffee
+        cup.setAttribute(
+            "title",
+            `A ${this.size} ${this.isDecaf ? "decaffinated" : "caffinated"} Coffee`,
+        );
 
-// Generate a description of the coffee and put it into the IMG title attribute
-
-// Insert the new IMG element into the paragraph
+        // Insert the new IMG element into the paragraph
+        output.appendChild(cup);
+    }
+}
 
 /* STEP 2: Instatiate a coffee based on the above constructor function */
+let priyanshCoffee = new Coffee("medium", false);
+let robertCoffee = new Coffee("large", true);
+let meganCoffee = new Coffee("small", false);
 
 /* STEP 3: Add a method to the Coffee class called serveIt() */
 
 /* STEP 4: Call up the serveIt() method */
+priyanshCoffee.serveIt();
+robertCoffee.serveIt();
+meganCoffee.serveIt();
 
 /* STEP 5: Define a subclass of the Coffee class */
 
